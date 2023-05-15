0cd438667b85abc56c6c9d4bbc2d8da5c3ccf51f
Yam-BS <yam_b777@outlook.com>
2023-05-15 07:57:11 -0300

    first commit

---
+++ src/test/java/tec/yam/livraria/LivrariaApplicationTests.java
@@ -0,0 +1,13 @@
+package tec.yam.livraria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivrariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
